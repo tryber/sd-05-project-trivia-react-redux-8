8d57f3f0cb20a4b7f60e54e4311a1f6e1ec1512b
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-08-20 21:31:56 -0300

    and now taking what Giu began to do

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,39 +1,43 @@
 import React from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import Header from './Header';
 
 class Game extends React.Component {
   render() {
-    // const { dataQuestion, dataCorrectAnswer, myFunction } = this.props
+    // const { dataCategory, dataQuestion, dataCorrectAnswer, dataWrongAnswer, fetchTrivia } = this.props;
+    const { dataGame } = this.props;
     return (
       <div>
         <Header />
-        {/* Pergunta e suas alternativas (todo interdependente até 8.)
-        Recebidas da API do Trivia em ordem aleatoria, usar button para alternativas.
-        Só deve ser possível escolher uma resposta correta por pergunta
-        Ao clicar em uma resposta, a resposta correta deve ficar verde e as incorretas, vermelhas
-        A pessoa que joga tem 30 segundos para responder cada pergunta
-        Ao clicar na resposta correta, pontos devem ser somados no placar da pessoa que está jogando
-        Após a resposta ser dada, o botão "Próxima" deve aparecer
-        A pessoa que joga deve responder 5 perguntas no total  */}
+        <section>
+          {/* <p data-testid="question-category">{dataCategory}</p>
+          <p data-testid="question-text">{dataQuestion}</p>
+          <div>
+            {dataWrongAnswer.map((answer, index) => (<button data-testid={`wrong-answer-${index}`}>{dataWrongAnswer[index]}</button>))}
+          </div>
+          <button data-testid="correct-answer">{dataCorrectAnswer}</button> */}
+          {dataGame}
+        </section>
       </div>
     );
   }
 }
 
-// const mapStateToProps = (state) => ({
 // para acessar o objeto do reducer fetchTrivia
 // que tem a chave dataGame cujo valor é recebido pela api
 // sabendo que dataGame é um array de 5 objetos dentro dos quais tem perguntas e respostas
 // Sugestao:
-// dataQuestion: state.fetchTrivia.dataGame.results.questions,
-// dataCorrectAnswer: state.fetchTrivia.dataGame.results.correct_answers,
-
+const mapStateToProps = (state) => ({
+  dataGame: state.fetchTrivia.dataGame,
+  // dataCategory: state.fetchTrivia.dataGame.results.category,
+  // dataQuestion: state.fetchTrivia.dataGame.results.question,
+  // dataCorrectAnswer: state.fetchTrivia.dataGame.results.correct_answer,
+  // dataWrongAnswer: state.fetchTrivia.dataGame.results.incorrect_answer[0]
+});
 
 // const mapDispatchToProps = (dispatch) => ({
 //   // myFunction: (e) => dispatch(myAction(e))
 // });
 
-// export default connect(mapStateToProps, mapDispatchToProps)(Game);
-
-export default Game;
+export default connect(mapStateToProps, null)(Game);
+// export default Game;
